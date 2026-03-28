7c5b179b25a0bb85756fb5e96552bfd1ef81cce5
dameesco-jpg <dameesco@gmail.com>
2026-03-27 21:13:10 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 import Navigation from '@/components/ui/Navigation'
 import Footer from '@/components/ui/Footer'
 import Hero from '@/components/marketing/Hero'
+import StatsSection from '@/components/marketing/StatsSection'
 import Features from '@/components/marketing/Features'
-import HowItWorks from '@/components/marketing/HowItWorks'
-import SocialProof from '@/components/marketing/SocialProof'
-import ServicesSection from '@/components/marketing/ServicesSection'
+import Testimonials from '@/components/marketing/Testimonials'
 import PricingSection from '@/components/marketing/PricingSection'
+import CustomMusic from '@/components/marketing/CustomMusic'
 import CTASection from '@/components/marketing/CTASection'
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-background">
       <Navigation />
       <Hero />
+      <StatsSection />
       <Features />
-      <HowItWorks />
-      <SocialProof />
-      <ServicesSection />
+      <Testimonials />
       <PricingSection />
+      <CustomMusic />
       <CTASection />
       <Footer />
     </main>
