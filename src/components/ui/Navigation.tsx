3f9f969b405450c3be4ca279a8d84766a4de1da4
dameesco-jpg <dameesco@gmail.com>
2026-03-28 14:32:39 -0400

    Update Navigation.tsx

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -18,9 +18,9 @@ export default function Navigation() {
             <Image
               src="/BrandMusic_H.png"
               alt="Brandmusic"
-              width={160}
-              height={40}
-              className="h-8 w-auto"
+              width={200}
+              height={50}
+              className="h-10 w-auto"
               priority
             />
           </Link>
